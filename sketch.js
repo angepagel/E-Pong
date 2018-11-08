e196bb95f1165a5ead37dc62e6cd7faf5a592e67
Ange Pagel <inkandev@gmail.com>
2018-11-08 06:56:44 +0100

    Add P1 and P2 paddles into the game

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
 
+let paddleP1;
+let paddleP2;
+
 function setup() {
+  
   createCanvas(window.innerWidth, window.innerHeight);
   frameRate(60);
+  
+  middle = window.innerHeight/2 - Paddle.height/2; // Middle of the window
+  paddleMargin = 30; // Margin between the window and paddles
+
+  /* Create and position Player 1 and Player 2 paddles */
+  paddleP1 = new Paddle(paddleMargin, middle);
+  paddleP2 = new Paddle(window.innerWidth-paddleMargin-Paddle.width, middle);
+
 }
 
 function draw() {
+
   background('#222222');
+
+  paddleP1.move(90, 83); // Player 1 controls are Z(up) and S(down) (azerty)
+  paddleP2.move(UP_ARROW, DOWN_ARROW); // Player 2 controls are the up and down arrows
+  
+  paddleP1.draw();
+  paddleP2.draw();
+
 }

11343d3e9cc7ad2a420c4f16d1269f6ffa3bf432
Inkan <inkandev@gmail.com>
2018-11-14 14:59:54 +0100

    Implement scoring system

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@
 let paddleP1;
 let paddleP2;
 let ball;
+let scoreP1;
+let scoreP2;
 
 function setup() {
   
@@ -11,9 +13,13 @@ function setup() {
   middle = window.innerHeight/2 - Paddle.height/2; // Middle of the window
   paddleMargin = 30; // Margin between the window and paddles
 
-  /* Create and position Player 1 and Player 2 paddles */
+  /* Creates and positions Player 1 and Player 2 paddles */
   paddleP1 = new Paddle(paddleMargin, middle);
   paddleP2 = new Paddle(window.innerWidth-paddleMargin-Paddle.width, middle);
+
+  /* Initializes both players' score */
+  scoreP1 = 0;
+  scoreP2 = 0;
 
   /* Creates and positions the ball at the center of the window */
   ball = new Ball(window.innerWidth/2-Ball.radius, window.innerHeight/2-Ball.radius);
@@ -36,6 +42,19 @@ function draw() {
   if (ball.position.y < 0 || ball.position.y + Ball.diameter > window.innerHeight) {
     ball.invertSpeedY();
   }
+
+  /* Ball collision with left border */
+  if (ball.position.x + Ball.diameter < 0) {
+    scoreP1++;
+    ball.reset();
+  }
+
+  /* Ball collision with right border */
+  if (ball.position.x > window.innerWidth) {
+    scoreP2++;
+    ball.reset();
+  }
+
 
   paddleP1.draw();
   paddleP2.draw();

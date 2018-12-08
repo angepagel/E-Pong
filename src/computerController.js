1ae1945b01aec5d0fb0d8c20ab972b34f3acafe6
Ange Pagel <inkandev@gmail.com>
2018-12-08 05:12:05 +0100

    Create ComputerController class

---
+++ src/computerController.js
@@ -0,0 +1,13 @@
+
+class ComputerController extends Controller {
+
+    constructor(paddle, ball) {
+        super(paddle);
+        this.ball = ball;
+    }
+
+    update() {
+        return
+    }
+
+}

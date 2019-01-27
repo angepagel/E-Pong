c654a8d9f6942cbd8b0e724f3f4841fe2192bad8
Ange Pagel <inkandev@gmail.com>
2019-01-27 22:17:25 +0100

    Add basic artificial intelligence

--- src/controllers/computerController.js
+++ src/controllers/computerController.js
@@ -3,11 +3,17 @@ class ComputerController extends Controller {
 
     constructor(paddle, ball) {
         super(paddle);
-        this.ball = ball;
+        this._paddle = this._gameObject;
+        this._ball = ball;
     }
 
     update() {
-        return
+        if (this._ball.position.y > this._paddle.position.y) {
+            this._paddle.goDown();
+        }
+        if (this._ball.position.y < this._paddle.position.y) {
+            this._paddle.goUp();
+        }
     }
 
 }

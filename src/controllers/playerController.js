797cbaf29dbfe5e0dd802e7d90f999cbb4158025
Ange Pagel <inkandev@gmail.com>
2019-01-27 21:57:44 +0100

    Update playerController.js

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -1,8 +1,8 @@
 
-class PlayerController extends Controller {
+class PlayerController {
 
     constructor(paddle, player) {
-        super(paddle);
+        this._paddle = paddle;
         if (player === 'P1') {
             this.upKeyCode = 90;
             this.downKeyCode = 83;
@@ -14,13 +14,13 @@ class PlayerController extends Controller {
 
     update() {
         if (keyIsDown(this.upKeyCode)) {
-            if (this.paddle.position.y > 0) {
-                this.paddle.position.y -= this.paddle.speed;
+            if (this._paddle.position.y > 0) {
+                this._paddle.position.add(createVector(0, -this._paddle.paddleSpeed));
             }
         }
         if (keyIsDown(this.downKeyCode)) {
-            if (this.paddle.position.y + Paddle.height < window.innerHeight) {
-                this.paddle.position.y += this.paddle.speed;
+            if (this._paddle.position.y + this._paddle.height < window.innerHeight) {
+                this._paddle.position.add(createVector(0, this._paddle.paddleSpeed));
             }
         }
     }

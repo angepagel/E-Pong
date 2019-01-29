3b05fd5680a8efd70f89e5c04a5dcbaf98e2bbe4
Ange Pagel <inkandev@gmail.com>
2019-01-29 16:43:00 +0100

    Update setup method

--- src/pong.js
+++ src/pong.js
@@ -18,7 +18,40 @@ class Pong {
     }
 
     setup() {
-        return
+        createCanvas(window.innerWidth, window.innerHeight);
+        frameRate(60);
+
+        this._paddleP1 = new Paddle(0,0);
+        this._paddleP2 = new Paddle(0,0);
+        this._ball = new Ball(0,0);
+
+        this._paddleP1.initialPosition = {
+            x: 30,
+            y: window.innerHeight/2 - this._paddleP1.height/2
+        }
+
+        this._paddleP2.initialPosition = {
+            x: window.innerWidth - 30 - this._paddleP2.width,
+            y: window.innerHeight/2 - this._paddleP2.height/2
+        }
+
+        this._ball.initialPosition = {
+            x: window.innerWidth/2 - this._ball.radius,
+            y: window.innerHeight/2 - this._ball.radius
+        }
+
+        this._gameObjects.push(this._paddleP1, this._paddleP2, this._ball);
+
+
+        this._paddleP1.controller = new PlayerController(this._paddleP1, 'P1');
+
+        if (this._mode === 'vs') {
+            this._paddleP2.controller = new PlayerController(this._paddleP2, 'P2');
+        }
+        else {
+            this._paddleP2.controller = new ComputerController(this._paddleP2, this._ball);
+        }
+
     }
 
     draw() {

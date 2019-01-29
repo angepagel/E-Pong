e1dcf6d70735fa151df8eff2fc172cffe21db50a
Ange Pagel <inkandev@gmail.com>
2019-01-29 16:42:29 +0100

    Update pong.js

--- src/pong.js
+++ src/pong.js
@@ -2,13 +2,19 @@
 class Pong {
 
     constructor(mode) {
+        this._mode = mode;
+        this._gameObjects = [];
 
+        this._scoreP1 = this._scoreP2 = 0;
+
+        this._speedMultiplier = 1;
+        this._speedIncrement = 0.07;
     }
 
     preload() {
-        this.soundWall = loadSound('assets/wall.wav');
-        this.soundPaddle = loadSound('assets/paddle.wav');
-        this.soundScore = loadSound('assets/score.wav');
+        this._soundWall = loadSound('assets/wall.wav');
+        this._soundPaddle = loadSound('assets/paddle.wav');
+        this._soundScore = loadSound('assets/score.wav');
     }
 
     setup() {
